c36769accbb759e3bd264708999433cebfeb8262
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2022-01-29 11:22:04 -0300

    feat: implementa sleep time

--- src/requestHandler/stress.ts
+++ src/requestHandler/stress.ts
@@ -6,26 +6,47 @@ config({ silent: true })
 axios.defaults.baseURL = 'http://localhost:3333';
 
 let MAX_REQUESTS = 50
+let SLEEP_TIME = 1
 
 if (process.env.NUMBER_OF_REQUESTS) {
   MAX_REQUESTS = parseInt(process.env.NUMBER_OF_REQUESTS)
 }
 
-console.log(MAX_REQUESTS)
-
-const requests: Promise<any>[] = []
-
-for (let i = 0; i < MAX_REQUESTS; i++) {
-  requests.push(axios('/stress').then((res) => {
-    console.log({ 
-      i,
-      count: res.data.count
-    })
-  }))
+if (process.env.SLEEP_TIME) {
+  SLEEP_TIME = parseInt(process.env.SLEEP_TIME)
 }
-Promise.all(requests).then(res => {
-  console.log('success')
+
+console.log({
+  Type: 'Stress',
+  MAX_REQUESTS,
+  SLEEP_TIME
 })
-.catch((err) => {
-  console.log(err)
-})
+
+function sleep(ms: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function handle() {
+  const requests: Promise<any>[] = []
+
+  for (let i = 0; i < MAX_REQUESTS; i++) {
+    await sleep(SLEEP_TIME)
+
+    requests.push(axios('/stress').then((res) => {
+      console.log({ 
+        i,
+        count: res.data.count
+      })
+    }))
+  }
+  Promise.all(requests).then(res => {
+    console.log('All requests handled')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+}
+
+handle()
